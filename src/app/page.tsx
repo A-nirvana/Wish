d55985a8d36f03e0d22739c52bc86bf4039703fd
hirakRajbonshi <hrkraj0@gmail.com>
2024-08-22 22:48:32 +0530

    small fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 
-import GSquare from "@/components/gsapSquare"
-import { Pacifico } from "next/font/google"
-import { useRouter } from "next/navigation"
-import LandingBG from "@/components/LandingBG"
+import GSquare from "@/components/gsapSquare";
+import { Pacifico } from "next/font/google";
+import { useRouter } from "next/navigation";
+import LandingBG from "@/components/LandingBG";
+import Image from "next/image";
+import { useLayoutEffect } from "react";
+import gsap from "gsap";
 
 const pacifico = Pacifico({
   weight: "400",
@@ -11,24 +14,53 @@ const pacifico = Pacifico({
   subsets: ["latin"],
 });
 
-const Home :React.FC =()=>{
-  const router = useRouter()
+const Home: React.FC = () => {
+  useLayoutEffect(() => {
+    gsap.fromTo(
+      ".crown",
+      { y: 1000 },
+      { y: 0, x: 0, duration: 4, delay: 3.3, ease: "elastic" }
+    );
+
+    const t1 = gsap.timeline({ repeat: -1, delay: 7 });
+    t1.to(".crown", { x: 0, duration: 4 });
+  }, []);
+  const router = useRouter();
   return (
-    <main className={"h-screen flex justify-evenly overflow-hidden bg-gray-900 "+pacifico.className}>
+    <main
+      className={
+        "h-screen flex justify-evenly overflow-hidden bg-gray-900 " +
+        pacifico.className
+      }
+    >
       <section className="h-full flex justify-center items-center">
+        <Image
+          className="absolute top-[28.5%] z-30 left-[12%] opacity-83 crown -rotate-[20deg]"
+          src="/Crown.png"
+          width={150}
+          height={150}
+          alt="crown"
+        />
         <GSquare />
       </section>
       <section className="relative z-10 text-7xl h-full flex flex-col justify-center items-center text-center w-[40%]">
-        <p  className={pacifico.className}>Wish your friends or loved ones a Happy Birthday</p>
-        <button className="mt-12 bg-purple-600 hover:animate-pulse hover:bg-purple-500 rounded-3xl text-2xl px-4 py-2" onClick={()=>{
-          router.push("/create_wish")
-        }}>Create a Wish</button>
+        <p className={pacifico.className}>
+          Wish your friends or loved ones a Happy Birthday
+        </p>
+        <button
+          className="mt-12 bg-purple-600 hover:animate-pulse hover:bg-purple-500 rounded-3xl text-2xl px-4 py-2"
+          onClick={() => {
+            router.push("/create_wish");
+          }}
+        >
+          Create a Wish
+        </button>
       </section>
       <section className="absolute h-screen w-screen overflow-hidden top-0 right-0 blur -z-0 opacity-50">
         <LandingBG />
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/LandingBG.tsx
+++ src/components/LandingBG.tsx
@@ -1,34 +1,68 @@
-"use client"
+"use client";
 
 import { gsap } from "gsap";
+import Image from "next/image";
 import { useLayoutEffect, useRef } from "react";
 
 const LandingBG = () => {
-
   const app = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    gsap.fromTo(".belon0", {y:1000},{y:-1000, duration: 2, delay:2, ease: "none"})
-    gsap.fromTo(".belon1", {y:1000},{y:-1000, duration: 2, delay:2.7, ease: "none"})
-    gsap.fromTo(".belon2", {y:1000},{y:-1000, duration: 2, delay:3, ease: "none"})
-    gsap.fromTo(".belon3", {y:1000},{y:-1000, duration: 2, delay:2.85, ease: "none"})
+    gsap.fromTo(
+      ".belon0",
+      { y: 1000 },
+      { y: -1000, duration: 2, delay: 2, ease: "none" }
+    );
+    gsap.fromTo(
+      ".belon1",
+      { y: 1000 },
+      { y: -1000, duration: 2, delay: 2.7, ease: "none" }
+    );
+    gsap.fromTo(
+      ".belon2",
+      { y: 1000 },
+      { y: -1000, duration: 2, delay: 3, ease: "none" }
+    );
+    gsap.fromTo(
+      ".belon3",
+      { y: 1000 },
+      { y: -1000, duration: 2, delay: 2.85, ease: "none" }
+    );
 
-    gsap.fromTo(".kamera", {y:1000},{y:0,x:100, duration:4, delay:3.3, ease: "elastic"})
+    gsap.fromTo(
+      ".kamera",
+      { y: 1000 },
+      { y: 0, x: 100, duration: 4, delay: 3.3, ease: "elastic" }
+    );
 
-    const t1= gsap.timeline({repeat:-1, delay:7});
-    t1.to(".kamera",{x:300,duration:4})
-    t1.to(".kamera",{x:100,duration:4})
-  }, [])
-
+    const t1 = gsap.timeline({ repeat: -1, delay: 7 });
+    t1.to(".kamera", { x: 300, duration: 4 });
+    t1.to(".kamera", { x: 100, duration: 4 });
+  }, []);
 
   return (
     <main>
-    <div ref={app} className="flex justify-between absolute z-100">
-        {Array.from({length: 7}, (_, i) => (
-            <img key={i} className={"w-[15vw] belon"+ Math.abs(3-i)} src="/belon.png"/>))}
-    </div>
-        <div className="w-80 h-80 bg-[url('/kamera.png')] bg-cover kamera"></div>
+      <div ref={app} className="flex justify-between absolute z-100">
+        {Array.from({ length: 7 }, (_, i) => (
+          <Image
+            alt="loon"
+            height={40}
+            width={40}
+            key={i}
+            className={"w-[15vw] belon" + Math.abs(3 - i)}
+            src="/belon.png"
+          />
+        ))}
+      </div>
+      <Image
+        className="absolute left-[50%] translate-x-[-100%] kamera"
+        src="/kamera.png"
+        width={230}
+        height={230}
+      />
+
+      {/* <div className="w-80 h-80 bg-[url('/kamera.png')] bg-cover kamera"></div> */}
     </main>
-  )
-}
-export default LandingBG
+  );
+};
+export default LandingBG;
